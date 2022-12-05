97fd9e0cca4390e6e31ce4613234373f7ec26399
youkharoufi <y.kharoufi96@gmail.com>
2022-12-05 01:40:23 -0500

    FirstCommit

---
+++ DatingApp/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using DatingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
---
+++ DatingApp/Models/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.Models
+{
+    public class AppUser
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
